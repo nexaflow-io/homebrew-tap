48fd2b8945d041f12f87653a9e33b85e2320d3d0
kazuyoshiii <kazu.81081203@gmail.com>
2026-05-15 21:20:20 +0900

    Update sf formula to 0.3.8

--- Formula/sf.rb
+++ Formula/sf.rb
@@ -1,8 +1,8 @@
 class Sf < Formula
   desc "Agent-first CLI for the Signal Foundry data workspace"
   homepage "https://signal-foundry.app"
-  url "https://github.com/nexaflow-io/signal-foundry-cli-releases/releases/download/cli-v0.3.7/signal-foundry-cli-0.3.7.tar.gz"
-  sha256 "ba29dc52c76899813af88314eddb90c9aecac68d60e7eaa1fdb97d895399e1f0"
+  url "https://github.com/nexaflow-io/signal-foundry-cli-releases/releases/download/cli-v0.3.8/signal-foundry-cli-0.3.8.tar.gz"
+  sha256 "e54bac9785e5db01b06ecb9516e865fac4dbe9befc4fe1f78cb0670f656ec933"
   license "LicenseRef-Signal-Foundry"
 
   depends_on "node@22"
@@ -17,7 +17,7 @@ class Sf < Formula
   end
 
   test do
-    assert_match "0.3.7", shell_output("#{bin}/sf version")
+    assert_match "0.3.8", shell_output("#{bin}/sf version")
     assert_match "effectiveBaseUrl", shell_output("#{bin}/sf auth show --json")
   end
 end
